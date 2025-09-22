2584844ddf6957ac6d40b6bd6815f79a5d5db017
bunnystring <ziknaf_548@hotmail.com>
2025-09-21 21:26:15 -0500

    Add infra-groups-service project with basic server configuration

---
+++ src/main/java/com/infragest/infra_groups_service/InfraGroupsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.infragest.infra_groups_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfraGroupsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfraGroupsServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/infragest/infra_groups_service/InfraGroupsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infragest.infra_groups_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfraGroupsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
